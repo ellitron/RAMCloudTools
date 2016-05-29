82c67a893977f0bd785193d074a46dfdf27f829b
Jonathan Ellithorpe <jde@cs.stanford.edu>
2016-05-28 20:12:28 -0700

    Updated rcstat to use new PerfStats.

--- src/main/cpp/rcstat.cc
+++ src/main/cpp/rcstat.cc
@@ -20,6 +20,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 #include "ClusterMetrics.h"
 #include "Context.h"
@@ -52,137 +53,364 @@ using namespace RAMCloud;
 void
 parseStats(Buffer* rawData, std::vector<PerfStats>* results)
 {
-    results->clear();
-    uint32_t offset = sizeof(WireFormat::ServerControlAll::Response);
-    while (offset < rawData->size()) {
-        WireFormat::ServerControl::Response* header =
-                rawData->getOffset<WireFormat::ServerControl::Response>(offset);
-        offset += sizeof32(*header);
-        if ((header == NULL) ||
-                ((offset + sizeof32(PerfStats)) > rawData->size())) {
-            break;
-        }
-        uint32_t i = ServerId(header->serverId).indexNumber();
-        if (i >= results->size()) {
-            results->resize(i+1);
-        }
-        rawData->copy(offset, header->outputLength, &results->at(i));
-        offset += header->outputLength;
+  results->clear();
+  uint32_t offset = sizeof(WireFormat::ServerControlAll::Response);
+  while (offset < rawData->size()) {
+    WireFormat::ServerControl::Response* header =
+        rawData->getOffset<WireFormat::ServerControl::Response>(offset);
+    offset += sizeof32(*header);
+    if ((header == NULL) ||
+        ((offset + sizeof32(PerfStats)) > rawData->size())) {
+      break;
     }
+    uint32_t i = ServerId(header->serverId).indexNumber();
+    if (i >= results->size()) {
+      results->resize(i+1);
+    }
+    rawData->copy(offset, header->outputLength, &results->at(i));
+    offset += header->outputLength;
+  }
 }
-  
+ 
+/**
+ * Check if the vector contains a particular element.
+ *
+ * \param v
+ *      vector to search.
+ * \param e
+ *      element to search for.
+ * \return
+ *      true if found, false otherwise.
+ */
+bool
+contains(std::vector<std::string> &v, const char *e) {
+  std::string elem(e);
+  if (std::find(v.begin(), v.end(), elem) != v.end()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 int
 main(int argc, char *argv[])
 try
 {
-    int clientIndex;
-    int numClients;
-    string serverLocator;
-    int interval;
+  int interval;
+  std::string format;
 
-    // Set line buffering for stdout so that printf's and log messages
-    // interleave properly.
-    setvbuf(stdout, NULL, _IOLBF, 1024);
+  // Set line buffering for stdout so that printf's and log messages
+  // interleave properly.
+  setvbuf(stdout, NULL, _IOLBF, 1024);
 
-    // need external context to set log levels with OptionParser
-    Context context(false);
+  OptionsDescription clientOptions("PerfStats");
+  clientOptions.add_options()
 
-    OptionsDescription clientOptions("PerfStats");
-    clientOptions.add_options()
+    ("interval",
+     ProgramOptions::value<int>(&interval)->
+        default_value(1),
+     "Reporting interval for statistics in seconds (default: 1s).") 
+    ("format",
+     ProgramOptions::value<std::string>(&format)->
+         default_value("mup,Mup"),
+     "Format options for rcstat output. Options are comma-separated.\n"
+     "  Ro   - Total number of objects read.\n"
+     "  ro   - Per server number of objects read.\n"
+     "  Rob  - Total number of bytes in objects read.\n"
+     "  rob  - Per server number of bytes in objects read.\n"
+     "  Rkb  - Total number of bytes in keys read.\n"
+     "  rkb  - Per server number of bytes in keys read.\n"
+     "  Wo   - Total number of objects written.\n"
+     "  wo   - Per server number of objects written.\n"
+     "  Wob  - Total number of bytes in objects written.\n"
+     "  wob  - Per server number of bytes in objects written.\n"
+     "  Wkb  - Total number of bytes in keys written.\n"
+     "  wkb  - Per server number of bytes in keys written.\n"
+     "  Mc   - Total memory capacity in bytes.\n"
+     "  mc   - Per server memory capacity in bytes.\n"
+     "  Mu   - Total amount of memory used in bytes.\n"
+     "  mu   - Per server amount of memory used in bytes.\n"
+     "  Mup  - Total amount of memory used as a \% of capacity.\n"
+     "  mup  - Per server amount of memory used as a \% of capacity.\n"
+     "  Mf   - Total amount of memory free in bytes.\n"
+     "  mf   - Per server amount of memory free in bytes.\n"
+     "  Mfp  - Total amount of memory free as a \% of capacity.\n"
+     "  mfp  - Per server amount of memory free as a \% of capacity.\n"
+     "[default: mup,Mup]");
 
-        // These first two options are currently ignored. They're here so that
-        // this script can be run with cluster.py.
-        ("clientIndex",
-         ProgramOptions::value<int>(&clientIndex)->
-            default_value(0),
-         "Index of this client (first client is 0; currently ignored)")
-        ("numClients",
-         ProgramOptions::value<int>(&numClients)->
-            default_value(1),
-         "Total number of clients running (currently ignored)")
+  OptionParser optionParser(clientOptions, argc, argv);
 
-        ("serverLocator",
-         ProgramOptions::value<string>(&serverLocator),
-         "Location of the server to get stats for.")
+  string locator = optionParser.options.getExternalStorageLocator();
+  if (locator.size() == 0) {
+      locator = optionParser.options.getCoordinatorLocator();
+  }
 
-        ("interval",
-         ProgramOptions::value<int>(&interval)->
-            default_value(1),
-         "Reporting interval for statistics in seconds (default: 1s).");
-     
-    OptionParser optionParser(clientOptions, argc, argv);
-    context.transportManager->setSessionTimeout(
-            optionParser.options.getSessionTimeout());
-
-    LOG(NOTICE, "Connecting to %s",
-        optionParser.options.getCoordinatorLocator().c_str());
-
-    string locator = optionParser.options.getExternalStorageLocator();
-    if (locator.size() == 0) {
-        locator = optionParser.options.getCoordinatorLocator();
+  // Parse the format option for the output columns.
+  std::vector<std::string> columns; 
+  {
+    std::stringstream ss(format);
+    std::string col;
+    while (std::getline(ss, col, ',')) {
+      columns.push_back(col);
     }
-    RamCloud client(&context, locator.c_str(),
-            optionParser.options.getClusterName().c_str());
+  }
 
-    bool printHeader=true;
-    std::vector<PerfStats> prevStats;
-    std::vector<PerfStats> currStats;
-    while(true) {
-      Buffer statBuf;
-      client.serverControlAll(WireFormat::ControlOp::GET_PERF_STATS, NULL, 0, 
-          &statBuf);
-
-      parseStats(&statBuf, &currStats);
-      if (printHeader) {
-        for (size_t i = 0; i < currStats.size(); i++) {
-          PerfStats& p = currStats[i];
-          if (p.collectionTime == 0) {
-            continue;
-          }
-          char columnTitle[50];
-          sprintf(columnTitle, "%d.reads", i);
-          printf("%12s", columnTitle);
-          sprintf(columnTitle, "(+diff)");
-          printf("%12s", columnTitle); 
-        }
-        printf("\n");
-        printHeader=false;
-      } 
-
-      for (size_t i = 0; i < currStats.size(); i++) {
-        PerfStats& p = currStats[i];
-        if (p.collectionTime == 0) {
-          continue;
-        }
-        printf("%12d", p.readCount);
-
-        int diff = 0;
-        if (prevStats.size() == currStats.size()) {
-          diff = p.readCount - prevStats[i].readCount;
-        } 
-
-        char column[50];
-        sprintf(column, "(+ %d)", diff);
-        printf("%12s", column);
-      }
-      printf("\n");
-
-      prevStats = currStats;
-
-      Cycles::sleep((uint64_t)interval*1000000);
-    }
-    /*
-    Cycles::sleep(1000000);
-    Buffer afterStats;
+  RamCloud client(locator.c_str());
+  
+  std::vector<PerfStats> currStats;
+  {
+    Buffer statBuf;
     client.serverControlAll(WireFormat::ControlOp::GET_PERF_STATS, NULL, 0, 
-        &afterStats);
-    string stats = PerfStats::printClusterStats(&statBuf, &afterStats)
-        .c_str(); 
+        &statBuf);
+    parseStats(&statBuf, &currStats);
+  }
 
-    printf("%s", stats.c_str());
-    */
+  int colWidth = 12;
+  const char *colFmtStr = "%12s";
 
-    return 0;
+  /*
+   * Print column headers.
+   */
+  for (int i = 0; i < currStats.size(); i++) {
+    /*
+     * ro   - Per server number of objects read.
+     * rob  - Per server number of bytes in objects read.
+     * rkb  - Per server number of bytes in keys read.
+     * wo   - Per server number of objects written.
+     * wob  - Per server number of bytes in objects written.
+     * wkb  - Per server number of bytes in keys written.
+     * mc   - Per server memory capacity in bytes.
+     * mu   - Per server amount of memory used in bytes.
+     * mup  - Per server amount of memory used as a \% of capacity.
+     * mf   - Per server amount of memory free in bytes.
+     * mfp  - Per server amount of memory free as a \% of capacity.
+     */
+    if (contains(columns, "ro")) { printf(colFmtStr, "ro"); }
+    if (contains(columns, "rob")) { printf(colFmtStr, "rob"); }
+    if (contains(columns, "rkb")) { printf(colFmtStr, "rkb"); }
+    if (contains(columns, "wo")) { printf(colFmtStr, "wo"); }
+    if (contains(columns, "wob")) { printf(colFmtStr, "wob"); }
+    if (contains(columns, "wkb")) { printf(colFmtStr, "wkb"); }
+    if (contains(columns, "mc")) { printf(colFmtStr, "mc"); }
+    if (contains(columns, "mu")) { printf(colFmtStr, "mu"); }
+    if (contains(columns, "mup")) { printf(colFmtStr, "mup"); }
+    if (contains(columns, "mf")) { printf(colFmtStr, "mf"); }
+    if (contains(columns, "mfp")) { printf(colFmtStr, "mfp"); }
+  }
+
+  printf("\n");
+
+  /*
+   * Ro   - Total number of objects read.
+   * Rob  - Total number of bytes in objects read.
+   * Rkb  - Total number of bytes in keys read.
+   * Wo   - Total number of objects written.
+   * Wob  - Total number of bytes in objects written.
+   * Wkb  - Total number of bytes in keys written.
+   * Mc   - Total memory capacity in bytes.
+   * Mu   - Total amount of memory used in bytes.
+   * Mup  - Total amount of memory used as a \% of capacity.
+   * Mf   - Total amount of memory free in bytes.
+   * Mfp  - Total amount of memory free as a \% of capacity.
+   */
+  if (contains(columns, "Ro")) { printf(colFmtStr, "Ro"); }
+  if (contains(columns, "Rob")) { printf(colFmtStr, "Rob"); }
+  if (contains(columns, "Rkb")) { printf(colFmtStr, "Rkb"); }
+  if (contains(columns, "Wo")) { printf(colFmtStr, "Wo"); }
+  if (contains(columns, "Wob")) { printf(colFmtStr, "Wob"); }
+  if (contains(columns, "Wkb")) { printf(colFmtStr, "Wkb"); }
+  if (contains(columns, "Mc")) { printf(colFmtStr, "Mc"); }
+  if (contains(columns, "Mu")) { printf(colFmtStr, "Mu"); }
+  if (contains(columns, "Mup")) { printf(colFmtStr, "Mup"); }
+  if (contains(columns, "Mf")) { printf(colFmtStr, "Mf"); }
+  if (contains(columns, "Mfp")) { printf(colFmtStr, "Mfp"); }
+
+  printf("\n");
+
+  std::vector<PerfStats> prevStats;
+  while (true) {
+    sleep(interval);
+    
+    prevStats = currStats;
+
+    Buffer statBuf;
+    client.serverControlAll(WireFormat::ControlOp::GET_PERF_STATS, NULL, 0, 
+        &statBuf);
+    parseStats(&statBuf, &currStats);
+
+    uint64_t Ro = 0;
+    uint64_t Rob = 0;
+    uint64_t Rkb = 0;
+    uint64_t Wo = 0;
+    uint64_t Wob = 0;
+    uint64_t Wkb = 0;
+    uint64_t Mc = 0;
+    uint64_t Mu = 0;
+    uint64_t Mf = 0;
+    for (size_t i = 0; i < currStats.size(); i++) {
+      PerfStats& c = currStats[i];
+      PerfStats& p = prevStats[i];
+
+      if (c.collectionTime == 0) {
+        continue;
+      }
+     
+      uint64_t ro = c.readCount;
+      uint64_t rob = c.readObjectBytes;
+      uint64_t rkb = c.readKeyBytes;
+      uint64_t wo = c.writeCount;
+      uint64_t wob = c.writeObjectBytes;
+      uint64_t wkb = c.writeKeyBytes;
+      uint64_t mc = c.logMaxLiveBytes;
+      uint64_t mu = c.logLiveBytes;
+      uint64_t mup = (100 * mu) / mc;
+      uint64_t mf = c.logAppendableBytes;
+      uint64_t mfp = (100 * mf) / mc;
+
+      if (contains(columns, "ro")) { printf("%12u", ro); }
+      if (contains(columns, "rob")) { printf("%12u", rob); }
+      if (contains(columns, "rkb")) { printf("%12u", rkb); }
+      if (contains(columns, "wo")) { printf("%12u", wo); }
+      if (contains(columns, "wob")) { printf("%12u", wob); }
+      if (contains(columns, "wkb")) { printf("%12u", wkb); }
+      if (contains(columns, "mc")) { printf("%12u", mc); }
+      if (contains(columns, "mu")) { printf("%12u", mu); }
+      if (contains(columns, "mup")) { printf("%12u", mup); }
+      if (contains(columns, "mf")) { printf("%12u", mf); }
+      if (contains(columns, "mfp")) { printf("%12u", mfp); }
+
+      Ro += ro;
+      Rob += rob;
+      Rkb += rkb;
+      Wo += wo;
+      Wob += wob;
+      Wkb += wkb;
+      Mc += mc;
+      Mu += mu;
+      Mf += mf;
+    }
+
+    uint64_t Mup = (100 * Mu) / Mc;
+    uint64_t Mfp = (100 * Mf) / Mc;
+
+    if (contains(columns, "Ro")) { printf("%12u", Ro); }
+    if (contains(columns, "Rob")) { printf("%12u", Rob); }
+    if (contains(columns, "Rkb")) { printf("%12u", Rkb); }
+    if (contains(columns, "Wo")) { printf("%12u", Wo); }
+    if (contains(columns, "Wob")) { printf("%12u", Wob); }
+    if (contains(columns, "Wkb")) { printf("%12u", Wkb); }
+    if (contains(columns, "Mc")) { printf("%12u", Mc); }
+    if (contains(columns, "Mu")) { printf("%12u", Mu); }
+    if (contains(columns, "Mup")) { printf("%12u", Mup); }
+    if (contains(columns, "Mf")) { printf("%12u", Mf); }
+    if (contains(columns, "Mfp")) { printf("%12u", Mfp); }
+
+    printf("\n");
+  }
+
+  return 1;
+
+//  bool printHeader=true;
+//  std::vector<PerfStats> prevStats;
+//  std::vector<PerfStats> currStats;
+//  int mode = 1;
+//  while(true) {
+//    Buffer statBuf;
+//    client.serverControlAll(WireFormat::ControlOp::GET_PERF_STATS, NULL, 0, 
+//        &statBuf);
+//
+//    parseStats(&statBuf, &currStats);
+//    switch(mode) {
+//      case 1:
+//        if (printHeader) {
+//          for (size_t i = 0; i < currStats.size(); i++) {
+//            PerfStats& p = currStats[i];
+//            if (p.collectionTime == 0) {
+//              continue;
+//            }
+//            char columnTitle[50];
+//            sprintf(columnTitle, "%d.reads", i);
+//            printf("%12s", columnTitle);
+//            sprintf(columnTitle, "(+diff)");
+//            printf("%12s", columnTitle); 
+//          }
+//          printf("\n");
+//          printHeader=false;
+//        } 
+//
+//        for (size_t i = 0; i < currStats.size(); i++) {
+//          PerfStats& p = currStats[i];
+//          if (p.collectionTime == 0) {
+//            continue;
+//          }
+//          printf("%12d", p.readCount);
+//
+//          int diff = 0;
+//          if (prevStats.size() == currStats.size()) {
+//            diff = p.readCount - prevStats[i].readCount;
+//          } 
+//
+//          char column[50];
+//          sprintf(column, "(+ %d)", diff);
+//          printf("%12s", column);
+//        }
+//        printf("\n");
+//        break;
+//      case 2:
+//        if (printHeader) {
+//          for (size_t i = 0; i < currStats.size(); i++) {
+//            PerfStats& p = currStats[i];
+//            if (p.collectionTime == 0) {
+//              continue;
+//            }
+//            char columnTitle[50];
+//            sprintf(columnTitle, "%d.reads", i);
+//            printf("%12s", columnTitle);
+//            sprintf(columnTitle, "(+diff)");
+//            printf("%12s", columnTitle); 
+//          }
+//          printf("\n");
+//          printHeader=false;
+//        } 
+//
+//        for (size_t i = 0; i < currStats.size(); i++) {
+//          PerfStats& p = currStats[i];
+//          if (p.collectionTime == 0) {
+//            continue;
+//          }
+//          printf("%12d", p.readCount);
+//
+//          int diff = 0;
+//          if (prevStats.size() == currStats.size()) {
+//            diff = p.readCount - prevStats[i].readCount;
+//          } 
+//
+//          char column[50];
+//          sprintf(column, "(+ %d)", diff);
+//          printf("%12s", column);
+//        }
+//        printf("\n");
+//        break;
+//      default:
+//        break;
+//    }
+//
+//    prevStats = currStats;
+//
+//    Cycles::sleep((uint64_t)interval*1000000);
+//  }
+//  /*
+//  Cycles::sleep(1000000);
+//  Buffer afterStats;
+//  client.serverControlAll(WireFormat::ControlOp::GET_PERF_STATS, NULL, 0, 
+//      &afterStats);
+//  string stats = PerfStats::printClusterStats(&statBuf, &afterStats)
+//      .c_str(); 
+//
+//  printf("%s", stats.c_str());
+//  */
+//
+//  return 0;
 } catch (RAMCloud::ClientException& e) {
     fprintf(stderr, "RAMCloud exception: %s\n", e.str().c_str());
     return 1;
