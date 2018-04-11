b004329dcacaaebec455df70887ce45d29eb5a5b
Jonathan Ellithorpe <jde@cs.stanford.edu>
2018-04-10 18:12:16 -0600

    Removed unnecessary comments in TimeOp.java

--- src/main/java/org/ellitron/ramcloudtools/TimeOp.java
+++ src/main/java/org/ellitron/ramcloudtools/TimeOp.java
@@ -32,24 +32,6 @@ public class TimeOp
     + "  --version      Show version.\n"
     + "\n";
     
-//    private static final String navalfate =
-//    "Naval Fate.\n"
-//    + "\n"
-//    + "Usage:\n"
-//    + "  naval_fate ship new <name>...\n"
-//    + "  naval_fate ship <name> move <x> <y> [--speed=<kn>]\n"
-//    + "  naval_fate ship shoot <x> <y>\n"
-//    + "  naval_fate mine (set|remove) <x> <y> [--moored | --drifting]\n"
-//    + "  naval_fate (-h | --help)\n"
-//    + "  naval_fate --version\n"
-//    + "\n"
-//    + "Options:\n"
-//    + "  -h --help     Show this screen.\n"
-//    + "  --version     Show version.\n"
-//    + "  --speed=<kn>  Speed in knots [default: 10].\n"
-//    + "  --moored      Moored (anchored) mine.\n"
-//    + "  --drifting    Drifting mine.\n"
-//    + "\n";
     private static final Logger logger = Logger.getLogger(TimeOp.class);
     
     public static void main( String[] args ) {
